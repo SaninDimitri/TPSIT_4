72d1950d56d086f5c57d41ecedba9f3e237f405a
SaninDimitri <s_dim@email.it>
2017-01-03 10:59:18 +0100

    ItIstitutomontani4inaSanin.java

---
+++ ItIstitutomontani4inaSanin.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.istitutomontani.pkg4ina.sanin; 
+
+/**
+ *
+ * @author Studente
+ */
+public class ItIstitutomontani4inaSanin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // TODO code application logic here
+    }
+    
+}
