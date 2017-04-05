565f1426a22ff39aa25b61edce61afd1602bb593
Studente <Studente@PC-ma10>
2017-04-05 13:30:03 +0200

    Verifica di tpsit

---
+++ Retta/src/retta/Inizio.java
@@ -0,0 +1,11 @@
+package retta;
+
+public class Inizio {
+
+    public static void main(String[] args) {
+    WinRetta f = new WinRetta();
+    f.setVisible(true);
+    f.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ Retta/src/retta/Retta.java
@@ -0,0 +1,15 @@
+package retta;
+
+class Retta {
+    
+    double a,b,c;
+
+    
+    public Retta(double x,double y,double z){
+        this.a = x;
+        this.b = y;
+        this.c = z;
+    }
+    
+
+}
